ca285b486508a123d3a3b8ce8aa7d026aa04637e
Forrest Olson <forrestrolson@gmail.com>
2018-10-24 13:02:50 -0500

    Added favicon and updated app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-const navLinks = document.querySelectorAll('.nav-link');
+$(document).ready(function() {
 
-navLinks.forEach( link => {
-    link.addEventListener('click', (e) => {
+    // Add smooth scrolling on all links inside the navbar
+    $("#navbarText a").on('click', function(event) {
         let navMenu = document.querySelector('#navbarText');
-        if ( navMenu.classList.contains('show') ) {
-            navMenu.classList.remove('show');
-        }
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // hide menu
+            if ( navMenu.classList.contains('show') ) {
+                navMenu.classList.remove('show');
+            }
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top-50
+            }, 800, function() {
+
+            });
+        } // End if
     });
 });
